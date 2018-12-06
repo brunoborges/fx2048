2d33db99a0ad0d01373a563c2141d5f0b3d520de
Bruno Borges <bruno.borges@microsoft.com>
2018-12-06 12:34:58 -0800

    fix pipe

--- azure-jdk-install.ps1
+++ azure-jdk-install.ps1
@@ -10,4 +10,12 @@ Write-Host "Over and out."
 
 $url = "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_windows-x64_bin.zip"
 $output = "$Env:AGENT_WORKFOLDER\openjdk-11.0.1_windows-x64_bin.zip"
+
+Write-Host "JDK DOWNLOAD STARTED"
+
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
+
+Write-Host "JDK DOWNLOAD COMPLETE"
+
+Write-Host "AGENT_WORKFOLDER contents again:"
+gci $Env:AGENT_WORKFOLDER
