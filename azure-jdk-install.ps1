61f157dd0dce54b4278970df535617208145b8fc
Bruno Borges <bruno.borges@microsoft.com>
2018-12-06 12:08:28 -0800

    windows setup

---
+++ azure-jdk-install.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+
+Write-Host "Hello World from $Env:AGENT_NAME."
+Write-Host "My ID is $Env:AGENT_ID."
+Write-Host "AGENT_WORKFOLDER contents:"
+gci $Env:AGENT_WORKFOLDER
+Write-Host "AGENT_BUILDDIRECTORY contents:"
+gci $Env:AGENT_BUILDDIRECTORY
+Write-Host "BUILD_SOURCESDIRECTORY contents:"
+gci $Env:BUILD_SOURCESDIRECTORY
+Write-Host "Over and out."
+
+$url = "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_windows-x64_bin.zip"
+$output = "$Env:AGENT_WORKFOLDER\openjdk-11.0.1_windows-x64_bin.zip"
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
