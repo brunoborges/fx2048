4e29be7ae7937c01b6450d2806a80422c9d3226d
Bruno Borges <bruno.borges@microsoft.com>
2018-12-06 12:47:23 -0800

    pipeline

--- azure-jdk-install.ps1
+++ azure-jdk-install.ps1
@@ -9,7 +9,7 @@ gci $Env:BUILD_SOURCESDIRECTORY
 Write-Host "Over and out."
 
 $url = "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_windows-x64_bin.zip"
-$output = "$Env:AGENT_WORKFOLDER\openjdk-11.0.1_windows-x64_bin.zip"
+$output = "$Env:AGENT_BUILDDIRECTORY\openjdk-11.0.1_windows-x64_bin.zip"
 
 Write-Host "JDK DOWNLOAD STARTED"
 
@@ -17,5 +17,5 @@ Write-Host "JDK DOWNLOAD STARTED"
 
 Write-Host "JDK DOWNLOAD COMPLETE"
 
-Write-Host "AGENT_WORKFOLDER contents again:"
-gci $Env:AGENT_WORKFOLDER
+Write-Host "AGENT_BUILDDIRECTORY contents again:"
+gci $Env:AGENT_BUILDDIRECTORY
