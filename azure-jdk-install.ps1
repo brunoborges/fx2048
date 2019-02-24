1eeed989efe3ecd622a011d401ad308b41afaf46
Bruno Borges <bruno.borges@microsoft.com>
2019-02-23 16:40:55 -0800

    refactored build setup
    fix parameters
    
    fix job name
    
    use right parameter

--- azure-jdk-install.ps1
+++
@@ -1,7 +0,0 @@
-# Download JDK 11
-$url = "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_windows-x64_bin.zip"
-$output = "$Env:AGENT_TOOLSDIRECTORY\openjdk-11.0.1_windows-x64_bin.zip"
-
-Write-Host "JDK Download started ..."
-(New-Object System.Net.WebClient).DownloadFile($url, $output)
-Write-Host "JDK Download complete!"
