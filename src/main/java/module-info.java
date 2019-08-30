052701f6535252e8a1e4cd4dcb49631e0eb15ced
Bruno Borges <bruno.borges@microsoft.com>
2019-08-30 14:40:10 -0700

    fix bug with eclipse which also affects vscode

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,9 @@ module fxgame {
     requires java.base;
     requires java.logging;
 
-    requires transitive javafx.base;
-    requires transitive javafx.controls;
-    requires transitive javafx.graphics;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
 
     exports game2048;
 }
