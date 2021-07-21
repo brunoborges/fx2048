e2ce30653de27468738d85b5b43f3b752d00a8fe
Bruno Borges <bruno.borges@gmail.com>
2021-07-21 15:42:36 -0700

    use java 17

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
-module fxgame {
+module fx2048 {
     requires java.base;
     requires java.logging;
 
     requires javafx.base;
     requires javafx.controls;
+    requires javafx.media;
+    requires javafx.fxml;
     requires javafx.graphics;
 
     requires com.almasb.fxgl.all;
